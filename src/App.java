2682781f995d2c7ffe1832676e0b29fdaa8cee33
Sandervanheesch <sander@vanheesch.nl>
2026-01-20 14:09:59 +0100

    Worked further on features

--- src/App.java
+++ src/App.java
@@ -23,41 +23,42 @@ public class App {
             inputMain.nextLine();
 
             if (choiceMain == 1) {
-                
-                System.out.print("Write your title: ");
-                String titleInput = inputMain.nextLine();
-                
-
-                System.out.println("Write your text: ");
-                String textInput = inputMain.nextLine();
-                
-                
-                int highestNoteId = 0;
-
-                for (Note note : mainNotesList.getNotesList()) {
-                    if (note.getNoteId() > highestNoteId) {
-                        highestNoteId = note.getNoteId();
-                    }
-                }
-
-                mainNotesList.addNote(new Note(highestNoteId, titleInput, textInput));
+                choiceIsCreateNote(inputMain, mainNotesList);                
             }
 
             else if (choiceMain == 2) {
                 mainNotesList.showNotesList();
 
-                System.out.println("Do you want to select a specific note (type 'y') or go back (type anything else)");
+                System.out.println("\nDo you want to select a specific note (type 'y') or go back (type anything else)");
                 
                 String yOrNot = inputMain.nextLine();
-                if (yOrNot.equals('y')) {
-                    System.out.println("Type which note you want to select by typing the number of the note:");
+                if (yOrNot.equals("y")) {
+                    System.out.println("\nType which note you want to select by typing the number of the note shown in the list:");
                     int noteNumberSelected = inputMain.nextInt();
                     inputMain.nextLine();
 
-                    System.out.println("You have selected number " + noteNumberSelected + " Which is note titled " + mainNotesList.getNote(noteNumberSelected-1).getTitle());
+                    Note specificNoteSelected = mainNotesList.getNote(noteNumberSelected-1);
+
+                    System.out.println("You have selected number " + noteNumberSelected + " Which is note titled " + specificNoteSelected.getTitle());
+
+                    System.out.println("\n************************");
+                    System.out.println("Press the corresponding number for what action you want to perform:");
+                    System.out.println("1. Read note");
+                    System.out.println("2. Delete note");
+                    System.out.println("3. Update note");
+                    System.out.println("0. Go back");
+                    System.out.println("************************");
+
+                    int specificNoteChoice = inputMain.nextInt();
+                    inputMain.nextLine();
+
+                    if (specificNoteChoice == 1) {
+                        System.out.println("\nNow showing your selected note");
+                        System.out.println("\n" + specificNoteSelected.getTitle());
+                        System.out.println("\n" + specificNoteSelected.getText());
+                    }
 
                 }
-                int indexSelectedNote = inputMain.nextInt();
             }
 
             else if (choiceMain == 0) {
@@ -75,4 +76,25 @@ public class App {
 
         
     }
+
+    public static void choiceIsCreateNote(Scanner scanner, NotesList mainNotesList) {
+        System.out.print("Write your title: ");
+                String titleInput = scanner.nextLine();
+                
+
+                System.out.println("Write your text: ");
+                String textInput = scanner.nextLine();
+                
+                
+                int highestNoteId = 0;
+
+                for (Note note : mainNotesList.getNotesList()) {
+                    if (note.getNoteId() > highestNoteId) {
+                        highestNoteId = note.getNoteId();
+                    }
+                }
+
+        mainNotesList.addNote(new Note(highestNoteId, titleInput, textInput));
+    }
+
 }
